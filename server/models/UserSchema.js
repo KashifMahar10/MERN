017810b90baf8fde8f774df5e3f66d441b37dd08
KashifMahar10 <ahmedkashif494u@example.com>
2023-04-05 23:08:29 -0700

    Backend completed

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -29,8 +33,50 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         },
+         tokens:
+        [
+        {
+         token:
+        {
+           type:String,
+            required:true
+        }
+
+        }
+     ]
 
 })
 
+
+// hasing password
+userSchema.pre('save', async function(next){
+    if(this.isModified('password'))
+    {
+        this.password = await bcrypt.hash(this.password,12)
+        this.cpassword = await bcrypt.hash(this.password,12)
+    }
+    next();
+});
+
+
+//Generating tokens
+userSchema.methods.generateAuthToken = async function(){
+    try {
+        let token = jwt.sign({_id: this._id}, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+        
+    } catch (error) {
+        
+    }
+
+}
+
+
 const User = mongoose.model('USER;',userSchema);
 module.exports = User;
+
+
+
+
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,15 +1,95 @@
-const express = require('express');
-// we need ROUTER for BACKEND so we will add the express router.
-const router = express.Router();
+            const express = require('express');
+            const bcrypt = require('bcrypt');
+            const jwt = require('jsonwebtoken');
+            // we need ROUTER for BACKEND so we will add the express router.
+            const router = express.Router();
 
-router.get('/',(req,res)=>{
-    console.log(":HELLO FROM ROUTER")
-});
-
-router.post('/register',(req,res)=>{
-    console.log(req.body);    
-
-})
+            require('../db/conn');
+            const User = require('../models/UserSchema');
 
 
-module.exports = router;
+            router.get('/',(req,res)=>{
+                console.log(":HELLO FROM ROUTER")
+            });
+
+            router.post('/register', async (req,res)=>{
+
+                const {name, email, phone , work, password, cpassword} = req.body;
+                if(!name || !email || !phone || !work || !password || !cpassword)
+                {
+                    return res.status(422).json({error: "Please fill all fileds"});
+                }
+                try {
+                    const userExist = await User.findOne({email:email})
+                    if(userExist)
+                    {
+                        return res.status(422).json({message:"Email already exist"})
+                    }
+                    
+                    const user = new User({name, email, phone , work, password, cpassword});
+                            
+                    await user.save();
+                    return res.status(201).json({message:"user Registered sucessfully"});
+                    
+                    
+                        //return res.status(500).json({message:"Failed to register"});
+                    
+                } catch (error) {
+                    console.log(error);
+                    
+                }
+            
+            
+            })
+
+
+        //Login Router
+
+        router.post('/login', async(req,res)=>{
+            // console.log(req.body)
+            // res.json({message:"Done"});
+            try {
+                let token;
+                const {email, password} = req.body;
+                if(!email || !password)
+                {
+                    return res.status(400).json({message:"Please fill all fields"});
+                }
+
+                const userLogin = await User.findOne({email:email})
+                if(userLogin)
+                {
+                 const isMatch = await bcrypt.compare(password, userLogin.password)
+                 //token
+                  token = await userLogin.generateAuthToken();
+                 console.log(token);
+
+                 //store cookie
+                 res.cookie("jwtoken", token, {
+                    expires: new Date(Date.now()+ 2589200000),
+                    httpOnly: true
+                 });
+
+                if(!isMatch)
+                {
+                    res.status(400).json({error:"Error in login"});
+                }
+                else
+                {
+                    res.json({message:"User login sucessfull"});
+
+                }
+                
+                }
+                else
+                {
+                    res.status(400).json({error:" Invalid login problem..!"}); 
+
+                }
+                
+            } catch (error) {
+            console.log(error) 
+            }
+        })
+
+            module.exports = router;
